6799d563c2393eee83ce0bb8fc5a5fbee5d7faf8
Krystian Kmiecik <okamster@o2.pl>
2020-02-29 15:27:35 +0100

    Fluent Builder

--- src/main/java/model/Auction.java
+++ src/main/java/model/Auction.java
@@ -3,7 +3,8 @@ package model;
 import java.time.LocalDateTime;
 
 public class Auction {
-    private int id;
+    private static int counter = 1;
+    private final int id;
 
     private String item;
     private String description;
@@ -14,8 +15,25 @@ public class Auction {
     private double highestBid;
     private Customer buyer;
 
-    public Auction(int id, String item, String description, LocalDateTime dueDate, Customer seller, double highestBid, Customer buyer) {
-        this.id = id;
+    public Auction() {
+this.id=counter++;
+    }
+
+    @Override
+    public String toString() {
+        return "Auction{" +
+                "id=" + id +
+                ", item='" + item + '\'' +
+                ", description='" + description + '\'' +
+                ", dueDate=" + dueDate +
+                ", seller=" + seller +
+                ", highestBid=" + highestBid +
+                ", buyer=" + buyer +
+                '}';
+    }
+
+    public Auction (String item, String description, LocalDateTime dueDate, Customer seller, double highestBid, Customer buyer) {
+        this.id = counter++;
         this.item = item;
         this.description = description;
         this.dueDate = dueDate;
@@ -24,12 +42,9 @@ public class Auction {
         this.buyer = buyer;
     }
 
+
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getItem() {
@@ -79,4 +94,57 @@ public class Auction {
     public void setBuyer(Customer buyer) {
         this.buyer = buyer;
     }
+
+    public static class AuctionBuilder {
+        private String item;
+        private String description;
+        private LocalDateTime dueDate;
+
+        private Customer seller;
+
+        private double highestBid;
+        private Customer buyer;
+
+        public AuctionBuilder withItem(String item) {
+            this.item = item;
+            return this;
+        }
+
+        public AuctionBuilder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public AuctionBuilder withDueDate(LocalDateTime dueDate) {
+            this.dueDate = dueDate;
+            return this;
+        }
+
+        public AuctionBuilder withSeller(Customer seller) {
+            this.seller = seller;
+            return this;
+        }
+
+        public AuctionBuilder withHighestBid(double highestBid) {
+            this.highestBid = highestBid;
+            return this;
+        }
+
+        public AuctionBuilder withBuyer(Customer buyer) {
+            this.buyer = buyer;
+            return this;
+        }
+        public Auction build(){
+            Auction auction = new Auction();
+            auction.buyer=buyer;
+            auction.description=description;
+            auction.dueDate=dueDate;
+            auction.highestBid=highestBid;
+            auction.item=item;
+            auction.seller=seller;
+            return auction;
+
+        }
+    }
 }
+
